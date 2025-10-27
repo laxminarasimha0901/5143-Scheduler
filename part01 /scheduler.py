90c5014b7026078c4e42cceb4d2e441faa92a51d
AndrewHuff6 <77467984+AndrewHuff6@users.noreply.github.com>
2025-10-26 21:08:09 -0500

    Add job arrival time handling in scheduler
    Added arrival_time attribute to Job class and implemented arrival checking in the scheduler.

--- part01 /scheduler.py
+++ part01 /scheduler.py
@@ -8,6 +8,7 @@ class Job:
     def __init__(self, job_id, bursts):
         self.id = job_id
         self.bursts = bursts  # e.g., [5, 3, 2]
+        self.arrival_time = arrival_time
         self.current_burst = 0
         self.state = "ready"
 
@@ -22,16 +23,41 @@ class Scheduler:
         self.wait = []
         self.cpu = []
         self.io = []
+        # jobs are sorted by their arrival times in ascending order, and
+        # puts these jobs in a new list called waiting_arrivals.
+        self.waiting_arrivals = sorted(jobs, key=lambda j: j.arrival_time)    
+
+    # -----------------------------------------------------
+    # Creates and holds a list of arrived jobs according to
+    # its tick. Method loops through waiting jobs, and will 
+    # move said job(s) to the ready queue. 
+    # -----------------------------------------------------
+    def check_arrivals(self):
+    """Move jobs that have arrived into the ready queue."""
+    arrived = []
+    
+    for job in self.waiting_arrivals:
+        if self.clock == job.arrival_time:
+            job.state = "ready"
+            self.ready.append(job)
+            arrived.append(job)
+    
+    # remove arrived jobs from waiting list
+    for job in arrived:
+        self.waiting_arrivals.remove(job)
 
     # -----------------------------------------------------
     def has_jobs(self):
         """Return True if any queue still has jobs."""
-        return any([self.ready, self.wait, self.cpu, self.io])
+        return any([self.ready, self.wait, self.cpu, self.io, self.waiting_arrivals])
 
     # -----------------------------------------------------
     def step(self):
         """Advance one tick of simulation logic."""
         self.clock += 1
+
+        # check for any new arrivals
+        self.check_arrivals()
 
         # Move jobs around randomly for demo (replace with FCFS/RR/etc.)
         if self.cpu:
