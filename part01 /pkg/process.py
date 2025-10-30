6659e0f8da0a144ac693f0cdd392efee6801b34e
Laxminarasimha Soma <laxminarasimha0901@gmail.com>
2025-10-29 21:36:06 -0500

    Implement Process class with burst management

---
+++ part01 /pkg/process.py
@@ -0,0 +1,60 @@
+# ---------------------------------------
+class Process:
+    """
+    Represents a process with CPU and I/O bursts
+    Attributes:
+        pid: unique process ID
+        bursts: list of bursts [{"cpu": X}, {"io": {"type": T, "duration": D}}, ...]
+        priority: scheduling priority (0 = highest)
+        state: current state ("new", "ready", "running", "waiting", "finished")
+    Methods:
+        current_burst(): returns the current burst or None if done
+        advance_burst(): moves to the next burst
+        __repr__(): string representation for debugging
+        __str__(): user-friendly string representation
+    """
+
+    def __init__(self, pid, bursts, priority=0):
+        """Initialize process with pid, bursts, and priority"""
+        self.pid = pid
+        self.bursts = bursts[:]  # [{"cpu": X}, {"io": {...}}, ...]
+        self.priority = priority
+        self.state = "new"
+
+        self.wait_time = 0  # Total time spent waiting in wait queue or ready queue
+        self.turnaround_time = 0  # Total time from arrival to completion
+        self.runtime = 0  # Total CPU time used
+        self.io_time = 0  # Total I/O time used
+        self.start_time = 0  # Time when the process started execution
+        self.end_time = 0  # Time when the process finished execution
+        self.init_cpu_bursts = 0
+        self.init_io_bursts = 0
+        for burst in bursts:
+            if "cpu" in burst:
+                self.init_cpu_bursts += burst["cpu"]
+            elif "io" in burst:
+                self.init_io_bursts += burst["io"]["duration"]
+        self.TotalBursts = self.init_cpu_bursts + self.init_io_bursts
+
+    def current_burst(self):
+        """Get the current burst"""
+        # Return the first burst if it exists, else None
+        return self.bursts[0] if self.bursts else None
+
+    def advance_burst(self):
+        """Move to the next burst"""
+        if self.bursts:
+            # Remove the first burst
+            self.bursts.pop(0)
+        # No return needed - modifies in place and current_burst() will reflect change
+
+    def __repr__(self):
+        # return self.__str__()
+        return f"{self.pid}"
+
+    def __str__(self):
+        return f"Process[pid:{self.pid}, priority:{self.priority}, runtime:{self.runtime}, io_time:{self.io_time}, start_time:{self.start_time}]"
+        # return self.__repr__()
+
+
+# ---------------------------------------
