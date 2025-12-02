3ee9de1de9066fb39cb7dbb38d4c06d57e649597
AndrewHuff6 <77467984+AndrewHuff6@users.noreply.github.com>
2025-12-02 02:11:00 +0000

    minor changes, issue with imports in main.py

--- part01 /main.py
+++ part01 /main.py
@@ -1,12 +1,11 @@
 # main.py
 import json
 import sys
-from pkg.process import Process
 from schedulers.fcfs import FCFSScheduler
 from schedulers.round_robin import RRScheduler
 from schedulers.sjf import SJFScheduler
-from schedulers.srjf import SRTFScheduler
-from pkg.process import Process
+from schedulers.srtf import SRTFScheduler
+from pkg import Process
 from visualizer import run_simulation_with_gantt
 
 def load_processes_from_json(filename, limit=None):
